3656978088b1285c6a2866dc6fb2175fbe07f705
bshah <bshah2016@gmail.com>
2016-10-02 13:21:45 -0400

    - Using Viewpager offset to change OnboardingActivity's background colors - updated colors - working on setting up OnboardingFragment

--- app/src/main/java/com/binitshah/hb141/OnboardingActivity.java
+++ app/src/main/java/com/binitshah/hb141/OnboardingActivity.java
@@ -1,23 +1,40 @@
 package com.binitshah.hb141;
 
+import android.animation.ValueAnimator;
 import android.graphics.Color;
+import android.os.Build;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.TypedValue;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.RelativeLayout;
+
+import me.relex.circleindicator.CircleIndicator;
 
 /*
 * OnboardingActivity provides the onboarding experience.
  */
-public class OnboardingActivity extends AppCompatActivity {
-    private static final int NUM_PAGES = 4;
+public class OnboardingActivity extends FragmentActivity {
     private ViewPager mPager;
     private PagerAdapter mPagerAdapter;
 
+    //Variables to mess with the background color
+    private RelativeLayout relativeLayout;
+    private int prevColor;
+    private int thisColor;
+    private int nextColor;
+    private int previousPage;
+    private float modPosition;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +44,44 @@ public class OnboardingActivity extends AppCompatActivity {
         mPager = (ViewPager) findViewById(R.id.pager);
         mPagerAdapter = new OnboardingPagerAdapter(getSupportFragmentManager());
         mPager.setAdapter(mPagerAdapter);
+        relativeLayout = (RelativeLayout) findViewById(R.id.activity_onboarding);
+        CircleIndicator indicator = (CircleIndicator) findViewById(R.id.indicator);
+        indicator.setViewPager(mPager);
+
+        prevColor = ContextCompat.getColor(this ,R.color.colorTransparent);
+        thisColor = ContextCompat.getColor(this ,R.color.colorOnboarding1);
+        nextColor = ContextCompat.getColor(this ,R.color.colorOnboarding2);
+        relativeLayout.setBackgroundColor(thisColor);
+
+        mPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                if (previousPage > position) {
+                    modPosition = (1 - positionOffset);
+                    if(modPosition >= 0.007){
+                        int blended = blendColors(thisColor, prevColor, modPosition);
+                        relativeLayout.setBackgroundColor(blended);
+                    }
+                    if(modPosition == 1){
+                        changeColors(position);
+                    }
+                } else {
+                    if(positionOffset <= 0.985) {
+                        int blended = blendColors(thisColor, nextColor, positionOffset);
+                        relativeLayout.setBackgroundColor(blended);
+                    }
+                    else {
+                        changeColors(position+1);
+                    }
+                }
+            }
+
+            @Override
+            public void onPageSelected(int position) {}
+
+            @Override
+            public void onPageScrollStateChanged(int state) {}
+        });
     }
 
     /**
@@ -34,29 +89,93 @@ public class OnboardingActivity extends AppCompatActivity {
      * sequence.
      */
     private class OnboardingPagerAdapter extends FragmentStatePagerAdapter {
+        private final int NUM_PAGES = 4;
+
         public OnboardingPagerAdapter(FragmentManager fm) {
             super(fm);
         }
 
         @Override
+        public int getCount() { return NUM_PAGES; }
+
+        @Override
         public Fragment getItem(int position) {
             switch (position) {
                 case 0:
-                    return OnboardingFragment.newInstance("Onboarding Screen 1", Color.parseColor("#E74C3C"));
+                    return OnboardingFragment.newInstance(1);
                 case 1:
-                    return OnboardingFragment.newInstance("Onboarding Screen 2", Color.parseColor("#F1C40F"));
+                    return OnboardingFragment.newInstance(2);
                 case 2:
-                    return OnboardingFragment.newInstance("Onboarding Screen 3", Color.parseColor("#1ABC9C"));
+                    return OnboardingFragment.newInstance(3);
                 case 3:
-                    return OnboardingFragment.newInstance("Onboarding Screen 4", Color.parseColor("#2ECC71"));
+                    return OnboardingFragment.newInstance(4);
                 default:
-                    return OnboardingFragment.newInstance("Incorrect Position", Color.parseColor("#FFFFFF"));
+                    return OnboardingFragment.newInstance(5);
             }
         }
+    }
 
-        @Override
-        public int getCount() {
-            return NUM_PAGES;
+    public void changeColors(int position){
+        previousPage = position;
+        switch (position){
+            case 0:
+                prevColor = ContextCompat.getColor(getApplicationContext(), R.color.colorTransparent);
+                thisColor = ContextCompat.getColor(getApplicationContext(), R.color.colorOnboarding1);
+                relativeLayout.setBackgroundColor(thisColor);
+                nextColor = ContextCompat.getColor(getApplicationContext(), R.color.colorOnboarding2);
+                break;
+            case 1:
+                prevColor = ContextCompat.getColor(getApplicationContext(), R.color.colorOnboarding1);
+                thisColor = ContextCompat.getColor(getApplicationContext(), R.color.colorOnboarding2);
+                relativeLayout.setBackgroundColor(thisColor);
+                nextColor = ContextCompat.getColor(getApplicationContext(), R.color.colorOnboarding3);
+                break;
+            case 2:
+                prevColor = ContextCompat.getColor(getApplicationContext(), R.color.colorOnboarding2);
+                thisColor = ContextCompat.getColor(getApplicationContext(), R.color.colorOnboarding3);
+                relativeLayout.setBackgroundColor(thisColor);
+                nextColor = ContextCompat.getColor(getApplicationContext(), R.color.colorOnboarding4);
+                break;
+            case 3:
+                prevColor = ContextCompat.getColor(getApplicationContext(), R.color.colorOnboarding3);
+                thisColor = ContextCompat.getColor(getApplicationContext(), R.color.colorOnboarding4);
+                relativeLayout.setBackgroundColor(thisColor);
+                nextColor = ContextCompat.getColor(getApplicationContext(), R.color.colorOnboarding5);
+                break;
+            case 4:
+                prevColor = ContextCompat.getColor(getApplicationContext(), R.color.colorOnboarding4);
+                thisColor = ContextCompat.getColor(getApplicationContext(), R.color.colorOnboarding5);
+                relativeLayout.setBackgroundColor(thisColor);
+                nextColor = ContextCompat.getColor(getApplicationContext(), R.color.colorOnboarding6);
+                break;
+            case 5:
+                prevColor = ContextCompat.getColor(getApplicationContext(), R.color.colorOnboarding5);
+                thisColor = ContextCompat.getColor(getApplicationContext(), R.color.colorOnboarding6);
+                relativeLayout.setBackgroundColor(thisColor);
+                nextColor = ContextCompat.getColor(getApplicationContext(), R.color.colorOnboarding7);
+                break;
+            case 6:
+                prevColor = ContextCompat.getColor(getApplicationContext(), R.color.colorOnboarding6);
+                thisColor = ContextCompat.getColor(getApplicationContext(), R.color.colorOnboarding7);
+                relativeLayout.setBackgroundColor(thisColor);
+                nextColor = ContextCompat.getColor(getApplicationContext(), R.color.colorTransparent);
+                break;
+            default:
+                prevColor = ContextCompat.getColor(getApplicationContext(), R.color.colorTransparent);
+                thisColor = ContextCompat.getColor(getApplicationContext(), R.color.colorTransparent);
+                relativeLayout.setBackgroundColor(thisColor);
+                nextColor = ContextCompat.getColor(getApplicationContext(), R.color.colorTransparent);
+                break;
         }
+    }
+
+    private int blendColors(int from, int to, float ratio) {
+        final float inverseRatio = 1f - ratio;
+
+        final float r = Color.red(to) * ratio + Color.red(from) * inverseRatio;
+        final float g = Color.green(to) * ratio + Color.green(from) * inverseRatio;
+        final float b = Color.blue(to) * ratio + Color.blue(from) * inverseRatio;
+
+        return Color.rgb((int) r, (int) g, (int) b);
     }
 }
--- app/src/main/java/com/binitshah/hb141/OnboardingFragment.java
+++ app/src/main/java/com/binitshah/hb141/OnboardingFragment.java
@@ -21,13 +21,6 @@ import android.widget.TextView;
  * create an instance of this fragment.
  */
 public class OnboardingFragment extends Fragment {
-    LinearLayout layout;
-    int content_color;
-    //ImageView circleImage;
-    TextView textView;
-    String content_text;
-
-    private OnFragmentInteractionListener mListener;
 
     /**
      * Use this factory method to create a new instance of
@@ -35,11 +28,9 @@ public class OnboardingFragment extends Fragment {
      *
      * @return A new instance of fragment OnboardingFragment.
      */
-    // TODO: Rename and change types and number of parameters
-    public static OnboardingFragment newInstance(/*int imageResId, */String text, int bgcolor) {
+
+    public static OnboardingFragment newInstance(int position) {
         OnboardingFragment fragment = new OnboardingFragment();
-        fragment.content_text = text;
-        fragment.content_color = bgcolor;
         return fragment;
     }
 
@@ -57,53 +48,7 @@ public class OnboardingFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_onboarding, container, false);
-        textView = (TextView) rootView.findViewById(R.id.content_text);
-        textView.setText(content_text);
-        //fragment.circleImage.setImageResource(imageResId);
-        layout = (LinearLayout) rootView.findViewById(R.id.linear_layout);
-        layout.setBackgroundColor(content_color);
-
         return rootView;
-    }
-
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
-/*
-    @Override
-    public void onAttach(Activity activity) {
-        super.onAttach(activity);
-        try {
-            mListener = (OnFragmentInteractionListener) activity;
-        } catch (ClassCastException e) {
-            throw new ClassCastException(activity.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-*/
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p/>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        public void onFragmentInteraction(Uri uri);
     }
 
 }
